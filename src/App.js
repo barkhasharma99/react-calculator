58057154cdbdcb02d4c0197abc9c36e33c0d4870
barkha sharma <barkha.sharma55722@gmail.com>
2022-01-07 15:19:22 +0530

    claculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+//import Calc from './Components/Calc';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [result, setresult] = useState("");
+  const handleClick = (event) => {
+    setresult(result.concat(event.target.value))
+  }
+  const calculate = () => {
+    setresult(eval(result).toString())
+
+  }
+  const clear = () => {
+    setresult("");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+    <div className='container '>
+      <h1 className='head'> CALCULATOR </h1>
+      <div className='calc'>
+        <input type="text" placeholder='0' id="answer" value={result}></input>
+        <input type="button" className='button' value="1" onClick={handleClick} />
+        <input type="button" className='button' value="2" onClick={handleClick} />
+        <input type="button" className='button' value="3" onClick={handleClick} />
+        <input type="button" className='button' value="c" onClick={handleClick} />
+        <input type="button" className='button' value="4" onClick={handleClick} />
+        <input type="button" className='button' value="5" onClick={handleClick} />
+        <input type="button" className='button' value="6" onClick={handleClick} />
+        <input type="button" className='button' value="+" onClick={handleClick} />
+        <input type="button" className='button' value="7" onClick={handleClick} />
+        <input type="button" className='button' value="8" onClick={handleClick} />
+        <input type="button" className='button' value="9" onClick={handleClick} />
+        <input type="button" className='button' value="-" onClick={handleClick} />
+        <input type="button" className='button' value="0" onClick={handleClick} />
+        <input type="button" className='button' value="." onClick={handleClick} />
+        <input type="button" className='button' value="/" onClick={handleClick} />
+        <input type="button" className='button' value="*" onClick={handleClick} />
+        <input type="button" className='button button1' value="=" onClick={calculate} />
+        <input type="button" className='button button1' value="clear" onClick={clear} />
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Components/Calc.js
@@ -0,0 +1,51 @@
+// import React,{useState} from 'react'
+// const App =()=>{
+//     const  [result, setResult] = useState("");
+    
+//     const handleClick = (e) =>{
+//       setResult(result.concat(e.target.name));
+//     }
+// }
+
+// export default function Calc() {
+//     return (
+//         <div>
+//             <div > Calculator</div>
+//             <table border="1">
+//                 <tr>
+//                     <td colspan="3"><input type="text" value={'result'} /></td>
+
+//                     <td><input type="button" value="c" onclick="clr()" /> </td>
+//                 </tr>
+//                 <tr>
+
+//                     <td><input type="button" name='1'  onclick={'handleClick'} /> </td>
+//                     <td><input type="button" value="2" onclick="dis('2')" /> </td>
+//                     <td><input type="button" value="3" onclick="dis('3')" /> </td>
+//                     <td><input type="button" value="/" onclick="dis('/')" /> </td>
+//                 </tr>
+//                 <tr>
+//                     <td><input type="button" value="4" onclick="dis('4')" /> </td>
+//                     <td><input type="button" value="5" onclick="dis('5')" /> </td>
+//                     <td><input type="button" value="6" onclick="dis('6')" /> </td>
+//                     <td><input type="button" value="-" onclick="dis('-')" /> </td>
+//                 </tr>
+//                 <tr>
+//                     <td><input type="button" value="7" onclick="dis('7')" /> </td>
+//                     <td><input type="button" value="8" onclick="dis('8')" /> </td>
+//                     <td><input type="button" value="9" onclick="dis('9')" /> </td>
+//                     <td><input type="button" value="+" onclick="dis('+')" /> </td>
+//                 </tr>
+//                 <tr>
+//                     <td><input type="button" value="." onclick="dis('.')" /> </td>
+//                     <td><input type="button" value="0" onclick="dis('0')" /> </td>
+
+//                     <td><input type="button" value="=" onclick="solve()" /> </td>
+//                     <td><input type="button" value="*" onclick="dis('*')" /> </td>
+//                 </tr>
+//             </table>
+//         </div>
+
+//     )
+// }
+
